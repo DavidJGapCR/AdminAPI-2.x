4292e8972f192a52022ff5931e3801cac299208b
David Jimenez <djimenez@growthaccelerationpartners.com>
2025-02-17 20:06:35 -0600

    tests

--- Application/EdFi.Ods.AdminApi.Common/Infrastructure/MultiTenancy/TenantIdentificationMiddleware.cs
+++ Application/EdFi.Ods.AdminApi.Common/Infrastructure/MultiTenancy/TenantIdentificationMiddleware.cs
@@ -85,9 +85,10 @@ public class TenantResolverMiddleware : IMiddleware
                 if (_options.Value.EnableAdminConsoleAPI)
                 {
                     if (!context.Request.Path.Value!.Contains("adminconsole/tenants") &&
-                    context.Request.Method != "GET")
+                    context.Request.Method != "GET" &&
+                    !context.Request.Path.Value.Contains("health", StringComparison.InvariantCultureIgnoreCase))
                     {
-
+                        ThrowTenantValidationError("Tenant header is missing (adminconsole)");
                     }
                 }
                 else if (!NonFeatureEndpoints())
